9501ff7e9092adc05430c0500d802c2de59afb9c
EC2 Default User <ec2-user@ip-172-31-23-8.us-west-1.compute.internal>
2022-10-15 13:17:27 +0000

    ジャンケン課題１回目

--- janken_ruby/janken.rb
+++ janken_ruby/janken.rb
@@ -12,7 +12,7 @@ cp_hand = rand(1..3)
 puts "自分#{hand_sign},相手:#{cp_hand}"
 
 case_pattern = ""
- 
+
 if(hand_sign == cp_hand)
   case_pattern = "パターンA"
   
@@ -22,65 +22,66 @@ elsif(hand_sign == 2 && cp_hand == 3)
   case_pattern = "パターンB"
 elsif(hand_sign == 3 && cp_hand == 1)
   case_pattern = "パターンB"
-  
+
 elsif(hand_sign == 1 && cp_hand == 3)
   case_pattern = "パターンC"
 elsif(hand_sign == 2 && cp_hand == 1)
   case_pattern = "パターンC"
 elsif(hand_sign == 3 && cp_hand == 2)
   case_pattern = "パターンC"
-
 else
-  case_pattern = "パターンD"
-
 end
 
 
  case case_pattern
   when "パターンA"
-   puts "あいこです。ジャンケンを終了します。"
-   
+    puts "あいこです。ジャンケンを終了します。"
+    
+    
   when "パターンB"
-   puts "あなたの勝ちです。あっちむいて..."
-   puts "1 : 上 , 2 : 下 , 3 : 左 , 4 : 右"
-    cp_finger = {上:1,下:2,左:3,右:4}
-    finger_sign = gets.to_i
-    cp_finger = rand(1..4)
-　　puts "自分:#{finger_sign},相手:#{cp_finger}"
-　　  finger_pattern = ""
-　　  if(finger_sign == cp_finger)
-　　   finger_pattern = "B-1"
-　　  else
-　　   finger_pattern = "B-2"
-　　  end
-　　  
-　　    case finger_pattern
-　　     when "B-1"
-　　       puts "完全勝利です!おめでとうございます!"
-　　      else
-　　      puts "惜しかったですね。もう１回初めからしましょう!"
-　　    end
-
-　 
-   when "パターンC"
-   puts "あなたの負けです。あっちむいて..."
-   puts "1 : 上 , 2 : 下 , 3 : 左 , 4 : 右"
+    puts "あなたの勝ちです。あっちむいて..."
+    puts "1 : 上 , 2 : 下 , 3 : 左 , 4 : 右"
+     
     cp_finger = {上:1,下:2,左:3,右:4}
     finger_sign = gets.to_i
     cp_finger = rand(1..4)
     puts "自分:#{finger_sign},相手:#{cp_finger}"
-　　 finger_pattern = ""
-　　 if(finger_pattern == cp_finger)
-　　  finger_pattern = "C-1"
-　　 else
-　　  finger_pattern = "C-2"
-　　    case finger_pattern
-　　     when "C-1"
-　　      puts "完全敗北です。ジャンケンを終了します"
-　　     when "C-2"
-　　      puts "危なかったですね。もう１回初めからしましょう!"
-         end
-　　 
-    when "パターンD"
-    puts "ジャンケンを終了します"
+    
+    u_pattern = ""
+    if(finger_sign == cp_finger)
+     u_pattern = "B1"
+    else
     end
+     
+　　 case u_pattern
+      when "B1"
+　　    puts "完全勝利です!おめでとうございます!"
+　　  else
+　　    puts "惜しかったですね。もう１回初めからしましょう"
+     end
+    
+　 
+  when "パターンC"
+    puts "あなたの負けです。あっちむいて..."
+    puts "1 : 上 , 2 : 下 , 3 : 左 , 4 : 右"
+    cp_finger = {上:1,下:2,左:3,右:4}
+    finger_sign = gets.to_i
+    cp_finger = rand(1..4)
+    puts "自分:#{finger_sign},相手:#{cp_finger}"
+    l_pattern = ""
+    if(finger_sign == cp_finger)
+     l_pattern = "C1"
+    else
+    end
+    
+     case l_pattern
+      when "C1"
+　      puts "完敗です。ジャンケンを終了します"
+　    else
+　      puts "危なかったですね。もう１回挑戦しましょう!"
+　   end
+　   
+　   
+  else
+    puts "ジャンケン終了"
+ end
