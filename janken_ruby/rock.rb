5067e7b98a572198e60cfe33832fb132fc166bdd
EC2 Default User <ec2-user@ip-172-31-23-8.us-west-1.compute.internal>
2022-10-18 13:25:17 +0000

    じゃんけん修正ループできるように

--- janken_ruby/rock.rb
+++ janken_ruby/rock.rb
@@ -1,7 +1,9 @@
+require "csv"
+
 puts "最初はグーじゃんけん・・・・"
 
 def janken
-  puts "0:グー , 1:チョキ , 2:パー , その他:やめる"
+puts "0:グー , 1:チョキ , 2:パー , その他:やめる"
 
   player_hand = gets.to_i
   program_hand = rand(0..2)
@@ -11,45 +13,72 @@ def janken
   
   if player_hand == program_hand
     puts "あいこで"
-    return true
+    @janken = "aiko"
+    aiko
     
   elsif(player_hand == 0 && program_hand == 1)||(player_hand == 1 && program_hand == 2)||(player_hand == 2 && program_hand == 0)
     puts "あなたの勝ちです"
     @janken = "win"
-    return false
+    attihoi
     
   elsif(player_hand == 0 && program_hand == 2)||(player_hand == 1 && program_hand == 0)||(player_hand == 2 && program_hand == 1)
     puts "あなたの負けです"
     @janken = "lose"
-    return false
+    attihoi
+  
+  elsif(@attihoi == "next_janken")&&(player_hand == 0 && program_hand == 1)||(player_hand == 1 && program_hand == 2)||(player_hand == 2 && program_hand == 0)
+    puts "あなたの勝ちです"
+    @janken = "win"
+    attihoi
+  
+  elsif(@attihoi == "next_janken")&&(player_hand == 0 && program_hand == 2)||(player_hand == 1 && program_hand == 0)||(player_hand == 2 && program_hand == 1)
+    puts "あなたの負けです"
+    @janken = "lose"
+    attihoi
     
   else
     puts "じゃんけんを終了します"
+    @janken = "finish"
+    attihoi
   end
-
 end
 
-next_game = true
 
+def aiko
+  next_game = true
 while next_game
   next_game = janken
 end
+end
 
-  puts "あっちむいて..."
-  puts "1 : 上 , 2 : 下 , 3 : 左 , 4 : 右"
- 
-  player_finger = gets.to_i
-  program_finger = rand(0..4)
-  fingers = ["上", "下","左","右"]
-  puts "あなたの手:#{fingers[player_finger]}, 相手の手:#{fingers[program_finger]}"
-    
-  if (@janken == "win")&&(player_finger == program_finger)
-    puts "あなたの勝ちです。勝利しました！"
-    
-  elsif (@janken == "lose")&&(player_finger == program_finger)
-    puts "あなたの負けです。どんまいです！"
+def attihoi
+  #あっち向いてホイ
+  if (@janken == "finish")
+    puts "ありがとうございました。"
     
   else
-    puts "もう一回初めからしましょう！"
-　  janken
+  puts "あっちむいて..."
+  puts "0 : 上 , 1 : 下 , 2 : 左 , 3 : 右"
+ 
+  player_finger = gets.to_i
+  program_finger = rand(0..3)
+  fingers = ["上","下","左","右"]
+  puts "あなたの手:#{fingers[player_finger]}, 相手の手:#{fingers[program_finger]}"
+    
+   if (@janken == "win")&&(player_finger == program_finger)
+     puts "あなたの勝ちです。勝利しました！"
+    return false
+    
+   elsif (@janken == "lose")&&(player_finger == program_finger)
+     puts "あなたの負けです。どんまいです！"
+    return false
+    
+   else
+     puts "もう一回初めからしましょう！"
+     @attihoi = "next_janken"
+     janken
+   end
   end
+end
+
+janken
